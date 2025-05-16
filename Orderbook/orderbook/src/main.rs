fbbcb5ce8b970e9b6d77a7c763f02cb857422fe8
Scriptize <dblaylock8915@gmail.com>
2025-05-16 15:04:37 -0400

    Testing Webhook

--- Orderbook/orderbook/src/main.rs
+++ Orderbook/orderbook/src/main.rs
@@ -1,3 +1,18 @@
+use std::time::Duration;
+
+
 fn main() {
-    println!("Hello, world!");
+    trpl::run(async {
+        trpl::spawn_task(async {
+            for i in 1..10 {
+                println!("hi number {i} from the first task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        });
+
+        for i in 1..5 {
+            println!("hi number {i} from the second task!");
+            trpl::sleep(Duration::from_millis(500)).await;
+        }
+    });
 }
