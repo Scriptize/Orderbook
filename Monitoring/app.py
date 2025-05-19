f9713085a02573e3d0c5d32dd3628e7289771f0b
B2SAUCEY <bblaylock8916@gmail.com>
2025-05-18 23:04:54 -0400

    deleted route

--- Monitoring/app.py
+++ Monitoring/app.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route("/data")
-def data_page():
-    return "This is data"
 
 if __name__ == '__main__':
     app.run(debug=True)
