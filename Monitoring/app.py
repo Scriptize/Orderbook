235554430f473ca7bf31fc7ca8c8fd1648727a60
B2SAUCEY <bblaylock8916@gmail.com>
2025-05-18 23:03:00 -0400

    Test Commit
    Added route for data

--- Monitoring/app.py
+++ Monitoring/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route("/data")
+def data_page():
+    return "This is data"
+
 if __name__ == '__main__':
     app.run(debug=True)
