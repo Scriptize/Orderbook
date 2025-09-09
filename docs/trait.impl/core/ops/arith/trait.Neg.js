5d8b1487b129a0875f0c420996454331ec64a529
Scriptize <dblaylock8915@gmail.com>
2025-09-09 13:20:26 -0400

    Added docs

---
+++ docs/trait.impl/core/ops/arith/trait.Neg.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["chrono",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/ops/arith/trait.Neg.html\" title=\"trait core::ops::arith::Neg\">Neg</a> for <a class=\"struct\" href=\"chrono/struct.TimeDelta.html\" title=\"struct chrono::TimeDelta\">TimeDelta</a>"]]],["jiff",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/ops/arith/trait.Neg.html\" title=\"trait core::ops::arith::Neg\">Neg</a> for <a class=\"struct\" href=\"jiff/struct.SignedDuration.html\" title=\"struct jiff::SignedDuration\">SignedDuration</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/ops/arith/trait.Neg.html\" title=\"trait core::ops::arith::Neg\">Neg</a> for <a class=\"struct\" href=\"jiff/struct.Span.html\" title=\"struct jiff::Span\">Span</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/ops/arith/trait.Neg.html\" title=\"trait core::ops::arith::Neg\">Neg</a> for <a class=\"struct\" href=\"jiff/struct.SpanFieldwise.html\" title=\"struct jiff::SpanFieldwise\">SpanFieldwise</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/ops/arith/trait.Neg.html\" title=\"trait core::ops::arith::Neg\">Neg</a> for <a class=\"struct\" href=\"jiff/tz/struct.Offset.html\" title=\"struct jiff::tz::Offset\">Offset</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[269,1033]}
